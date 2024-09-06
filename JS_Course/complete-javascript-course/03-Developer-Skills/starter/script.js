c7cd455f03795701897ffdb83458814c05efa94b
Vu Minh Hieu <hieu.vuminh1@vti.com.vn>
2024-09-06 16:50:45 +0700

    update

--- JS_Course/complete-javascript-course/03-Developer-Skills/starter/script.js
+++ JS_Course/complete-javascript-course/03-Developer-Skills/starter/script.js
@@ -1,3 +1,13 @@
-// Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+function printForecast(arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}°C in ${i + 1} days...`;
+  }
+  console.log('...' + str);
+}
+
+printForecast(temperatures);

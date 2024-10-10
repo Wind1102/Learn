f7a25f4770688f01ef01afbe9e775353b6a61a48
Wind1102 <Hieu.vm02@gmail.com>
2024-10-10 22:24:41 +0700

    update

--- JS_Course/complete-javascript-course/11-Arrays-Bankist/starter/script.js
+++ JS_Course/complete-javascript-course/11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,12 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+let count = 0;
+movements.forEach((mov) =>{
+  if(mov > 0){
+    count += 1;
+    console.log(`You deposited ${mov}`);
+  }
+})
+
+console.log(count);

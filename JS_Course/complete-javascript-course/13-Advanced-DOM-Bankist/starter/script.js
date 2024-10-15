74a5443971b1443fe9a55583367ab748952dfb04
Vu Minh Hieu <hieu.vuminh1@vti.com.vn>
2024-10-15 19:42:26 +0700

    update

--- JS_Course/complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
+++ JS_Course/complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (ev) {
+  ev.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -29,3 +30,15 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/////////////////////
+
+const message = document.createElement('div');
+
+message.classList.add('cookie-message');
+message.innerHTML =
+  'we use cookied for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>';
+
+const header = document.querySelector('.header')
+header.append(message);
+header.prepend(message.cloneNode(true));

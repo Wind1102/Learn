30051794ebe44217927004e92c83ac2ffb1d16e8
Wind1102 <Hieu.vm02@gmail.com>
2024-11-04 07:56:18 +0700

    update

---
+++ JS_Course/complete-javascript-course/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,17 @@
+//Importing module
+'use strict';
+
+// import './shoppingCart.js';
+// import { shippingCost, totalprice } from './shoppingCart.js';
+// import {
+//   totalQuantity,
+//   totalprice as price,
+// } from './shoppingCart.js';
+
+// console.log('Importing Module');
+// console.log(shippingCost);
+// addToCart('bread', 5);
+
+import * as shoppingCart from './shoppingCart.js';
+
+
